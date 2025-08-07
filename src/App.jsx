358c39099bc9a0b0ba31f1d0303752bae6bf7851
Lakshman76 <lakshmankumar2603@gmail.com>
2025-08-07 12:10:29 +0530

    navbar conditonal rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Features from "./pages/Features";
-import AuthPage from "./pages/AuthPage"
+import AuthPage from "./pages/AuthPage";
 import Documentaions from "./pages/Documentaions";
-
-
-
+import Dashboard from "./components/Dashboard";
+import RequireAuth from "./auth/RequireAuth";
 
 function App() {
+  const location = useLocation();
+  const hideNavbar = ["/dashboard"];
+  const isNavbarHide = hideNavbar.some((path) =>
+    location.pathname.startsWith(path)
+  );
   return (
-    <Router>
-      <Navbar />
+    <>
+      {!isNavbarHide && <Navbar />}
       <Routes>
-        <Route path="/" element={ <Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/features" element={<Features/>} />
-        <Route path="/docs" element={<Documentaions/>} />
-        <Route path="/get-started" element={<AuthPage />}/>
-        </Routes>
-    </Router>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/docs" element={<Documentaions />} />
+        <Route path="/get-started" element={<AuthPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
